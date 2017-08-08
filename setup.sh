492b4e5fa714f0256a12cdfc7d9a8ec1ef63e073
campbell <campbell@hilbert.campbellwinnie>
2017-08-08 23:32:14 +0800

    Add setup.sh for rcfile

---
+++ setup.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env sh
+
+OS=`uname -s`
+
+FILES=""
+FILES="$FILES .agignore"
+FILES="$FILES .bash_aliases"
+FILES="$FILES .bashrc"
+FILES="$FILES .cgdb"
+FILES="$FILES .cshrc"
+FILES="$FILES .dir_colors"
+FILES="$FILES .emacs"
+FILES="$FILES .gdbinit"
+FILES="$FILES .gitconfig"
+FILES="$FILES .gitignore_global"
+FILES="$FILES .profile"
+FILES="$FILES .screenrc"
+FILES="$FILES .tmux.conf"
+FILES="$FILES .urxvt"
+FILES="$FILES .Xdefaults"
+FILES="$FILES .Xresources"
+
+cd $HOME
+
+for FILE in $FILES; do
+    rm -vfr $FILE > /dev/null
+    ln -s $HOME/.rcfile/$FILE $HOME
+done
+case $OS in
+    "Linux")
+        rm -vfr $HOME/.config/htop > /dev/null
+        mkdir -p $HOME/.config/htop > /dev/null
+        ln -s $HOME/.rcfile/.htoprc $HOME/.config/htop/htoprc
+        ;;
+    "Darwin")
+        rm -vfr .htoprc > /dev/null
+        ln -s $HOME/.rcfile/.htoprc $HOME/.htoprc
+        ;;
+esac
+
+cd .rcfile
+
+echo "Done"
